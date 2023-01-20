0557a8b9ca29d80236cedc94934a31fa75dcf4e9
LYK <dalinaum@gmail.com>
2023-01-20 18:32:36 +0900

    Add card final

--- part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
+++ part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
@@ -3,19 +3,23 @@ package kr.co.fastcampus.part4.chapter3_12
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import kr.co.fastcampus.part4.chapter3_12.ui.theme.CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -65,6 +69,20 @@ fun CardEx(cardData: CardData) {
             // 스텝 2: `AsyncImage`에는 `placeholder`를 지정하고,
             // `contentScale`을 `ContentScale.Crop`으로 설정합시다.
             // `clip(CircleShape)`로 둥근 외양을 만들어 봅시다.
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(text = cardData.author)
+                Text(text = cardData.description)
+            }
         }
     }
 }
