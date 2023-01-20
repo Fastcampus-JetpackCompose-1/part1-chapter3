d08e331083bef022c6334fbfd88bf3547ca66592
LYK <dalinaum@gmail.com>
2023-01-20 11:07:43 +0900

    Add Slot final

--- part4-chapter3-16/app/src/main/java/kr/co/fastcampus/part4/chapter3_16/MainActivity.kt
+++ part4-chapter3-16/app/src/main/java/kr/co/fastcampus/part4/chapter3_16/MainActivity.kt
@@ -37,12 +37,60 @@ class MainActivity : ComponentActivity() {
 // 스텝 1: `Row`를 `@Composable` 함수로 분리합시다.
 // `checked`의 값, `Text`의 `text`를 인자로 전달합시다.
 
+//@Composable
+//fun checkboxWithText(checked: MutableState<Boolean>, text: String) {
+//    Row(verticalAlignment = Alignment.CenterVertically) {
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = { checked.value = it }
+//        )
+//        Text(
+//            text = text,
+//            modifier = Modifier.clickable { checked.value = !checked.value }
+//        )
+//    }
+//}
+
 // 스텝 2: `@Composable` 함수에서 `@Composable () -> Unit` 타입으로
 // `content`를 받아옵시다. `Row`의 `Text`를 뺴고 `content()`를 넣읍시다.
 // `Row`에 `Modifier.clickable`를 넣어 전체를 클릭가능하게 합시다.
 
+//@Composable
+//fun checkBoxWithContent(
+//    checked: MutableState<Boolean>,
+//    content: @Composable () -> Unit
+//) {
+//    Row(
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.clickable { checked.value = !checked.value }
+//    ) {
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = { checked.value = it },
+//        )
+//        content()
+//    }
+//}
+
 // 스텝 3: `content`의 타입을 `@Composable RowScope.() -> Unit`로
 // 바꿉시다. 이렇게 다른 콤포저블 컨텐트를 넣는 방식을 Slot API라 합니다.
+
+//@Composable
+//fun checkBoxWithContent(
+//    checked: MutableState<Boolean>,
+//    content: @Composable RowScope.() -> Unit
+//) {
+//    Row(
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.clickable { checked.value = !checked.value }
+//    ) {
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = { checked.value = it },
+//        )
+//        content()
+//    }
+//}
 
 // 스텝 4: 상태를 바꾸는 람다를 `@Composable` 함수의 인자로 뺍시다.
 // 인자에서 MutableState대신 boolean 값으로 변경합시다.
@@ -71,25 +119,59 @@ fun SlotEx() {
     val checked2 = remember { mutableStateOf(false) }
 
     Column {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(
-                checked = checked1.value,
-                onCheckedChange = { checked1.value = it }
-            )
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Checkbox(
+//                checked = checked1.value,
+//                onCheckedChange = { checked1.value = it }
+//            )
+//            Text(
+//                text = "텍스트 1",
+//                modifier = Modifier.clickable { checked1.value = !checked1.value }
+//            )
+//        }
+//
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Checkbox(
+//                checked = checked2.value,
+//                onCheckedChange = { checked2.value = it }
+//            )
+//            Text(
+//                text = "텍스트 2",
+//                modifier = Modifier.clickable { checked2.value = !checked2.value }
+//            )
+//        }
+
+        // 스텝 2
+//        checkboxWithText(checked1, "텍스트 1")
+//        checkboxWithText(checked2, "텍스트 2")
+
+        // 스텝 3
+//        checkBoxWithContent(checked = checked1) {
+//            Text(
+//                text = "텍스트 1"
+//            )
+//        }
+//        checkBoxWithContent(checked = checked2) {
+//            Text(
+//                text = "텍스트 2"
+//            )
+//        }
+
+        // 스텝 4
+        checkBoxWithContent(
+            checked = checked1.value,
+            toggleState = { checked1.value = !checked1.value }
+        ) {
             Text(
-                text = "텍스트 1",
-                modifier = Modifier.clickable { checked1.value = !checked1.value }
+                text = "텍스트 1"
             )
         }
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(
-                checked = checked2.value,
-                onCheckedChange = { checked2.value = it }
-            )
+        checkBoxWithContent(
+            checked = checked2.value,
+            toggleState = { checked2.value = !checked2.value }
+        ) {
             Text(
-                text = "텍스트 2",
-                modifier = Modifier.clickable { checked2.value = !checked2.value }
+                text = "텍스트 2"
             )
         }
     }
